8c44e2f0e33cd7aef79f9703edc0b3c69baffcc8
jalal <jalal@DESKTOP-RFI8G6D>
2016-07-01 21:37:38 -0400

    Bumped version

--- src/NLog.Targets.Fluentd/Properties/AssemblyInfo.cs
+++ src/NLog.Targets.Fluentd/Properties/AssemblyInfo.cs
@@ -15,5 +15,5 @@ using System.Runtime.InteropServices;
 
 [assembly: Guid("d5cd99d9-f71f-447b-889e-687a6a3f918c")]
 
-[assembly: AssemblyVersion("1.0.4.0")]
-[assembly: AssemblyFileVersion("1.0.4.0")]
+[assembly: AssemblyVersion("1.1.0.0")]
+[assembly: AssemblyFileVersion("1.1.0.0")]
