69eddfab1ffa7ec3a3e635a99703c779928c4922
Edwin Engelen <edwin@engelen.name>
2020-03-24 12:45:59 +0100

    Send fractions of a second to Fluentd
    Previously the timestamp did have a a precision of one second. The fraction got lost.
    
    The timestamp is now of the EventTime time according to specification:
    https://github.com/fluent/fluentd/wiki/Forward-Protocol-Specification-v1#eventtime-ext-format
    
    EventTime has nanosecond precision, but .NET only supports 100 nanoseconds (ticks).
    
    Fixes #12
    
    Signed-off-by: Edwin Engelen <edwin@engelen.name>

---
+++ src/NLog.Targets.Fluentd/PackerExtensions.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace NLog.Targets
+{
+    using MsgPack;
+
+    internal static class PackerExtensions
+    {
+        private const int nanoSecondsPerSecond = 1 * 1000 * 1000 * 1000;
+        private const double ticksToNanoSecondsFactor = (double)nanoSecondsPerSecond / TimeSpan.TicksPerSecond;
+        private static readonly long unixEpochTicks = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc).Ticks;
+
+        /// <summary>
+        /// Write according to Fluend EventTime specification.
+        /// </summary>
+        /// <remarks>
+        /// Specification: https://github.com/fluent/fluentd/wiki/Forward-Protocol-Specification-v1#eventtime-ext-format"
+        /// </remarks>
+        public static Packer PackEventTime(this Packer that, DateTime value)
+        {
+            DateTimeToEpoch(value, out var secondsFromEpoch, out var nanoSeconds);
+
+            that.PackExtendedTypeValue(
+                0x0,
+                new[]
+                {
+                    (byte) ((ulong) (secondsFromEpoch >> 24) & (ulong) byte.MaxValue),
+                    (byte) ((ulong) (secondsFromEpoch >> 16) & (ulong) byte.MaxValue),
+                    (byte) ((ulong) (secondsFromEpoch >> 8) & (ulong) byte.MaxValue),
+                    (byte) ((ulong) secondsFromEpoch & (ulong) byte.MaxValue),
+                    (byte) ((ulong) (nanoSeconds >> 24) & (ulong) byte.MaxValue),
+                    (byte) ((ulong) (nanoSeconds >> 16) & (ulong) byte.MaxValue),
+                    (byte) ((ulong) (nanoSeconds >> 8) & (ulong) byte.MaxValue),
+                    (byte) ((ulong) nanoSeconds & (ulong) byte.MaxValue),
+                });
+
+            return that;
+        }
+
+        private static void DateTimeToEpoch(DateTime value, out uint secondsFromEpoch, out uint nanoSeconds)
+        {
+            var fromEpochTicks = value.ToUniversalTime().Ticks - unixEpochTicks;
+            secondsFromEpoch = (uint)(fromEpochTicks / TimeSpan.TicksPerSecond);
+            nanoSeconds = (uint)((fromEpochTicks - secondsFromEpoch * TimeSpan.TicksPerSecond) * ticksToNanoSecondsFactor);
+        }
+    }
+}
